ad4f725a256378b5265437c4ab3580b0c1a74ba0
Rachel Okimoto <rachel.okimoto@gmail.com>
2024-10-28 16:21:49 -0700

    fixes provider import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next';
 import GlobalStyles from './styles/global';
-import Providers from './Providers';
+import Providers from './providers';
 
 export const metadata: Metadata = {
   title: 'Rachel Okimoto Portfolio',
   description:
-    'Rachel Okimoto is a senior front-end web developer with 9+ years of experience.',
+    'Rachel Okimoto is a senior frontend web developer with 9+ years of experience.',
 };
 
 export default function RootLayout({
